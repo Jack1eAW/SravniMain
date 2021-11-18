17690676aeb9f2106f58b21d91890d95109e3ce0
Jack1eAW <59226187+Jack1eAW@users.noreply.github.com>
2021-11-18 14:32:57 +0300

    Initial Commit

---
+++ SravniMain/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SravniMain
+//
+//  Created by Aleksandr Chebotarev on 18.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SravniMain/SravniMainApp.swift
@@ -0,0 +1,17 @@
+//
+//  SravniMainApp.swift
+//  SravniMain
+//
+//  Created by Aleksandr Chebotarev on 18.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct SravniMainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
